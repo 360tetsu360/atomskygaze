6bf8655503efe21fb792fb09a3c1efa659b52f64
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2024-05-09 21:43:18 +0000

    Fix build script

--- isvp-sys/build.rs
+++ isvp-sys/build.rs
@@ -1,9 +1,13 @@
 fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
+    
+    let build_dir = env::var("GITHUB_WORKSPACE").unwrap_or("/atomskygaze".to_string());
+    let src_dir = env::var("GITHUB_WORKSPACE").unwrap_or("/src".to_string());
     println!(
-        "cargo:rustc-link-search=native=/atomskygaze/build/buildroot-2024.02/output/target/usr/lib"
+        "cargo:rustc-link-search=native={}/build/buildroot-2024.02/output/target/usr/lib",
+        build_dir
     );
-    println!("cargo:rustc-link-search=native=/src/mxu");
+    println!("cargo:rustc-link-search=native={}/mxu", src_dir);
     //println!("cargo:rustc-link-lib=uClibc");
     //println!("cargo:rustc-link-lib=lduClibc");
     //println!("cargo:rustc-link-lib=udl");
