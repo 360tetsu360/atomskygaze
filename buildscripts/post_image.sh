4c6c7709edde5360e3d56ce23b3176eb4d652577
360tetsu360 <tetsuhiko.game.minecraft@gmail.com>
2024-05-28 14:42:24 +0000

    Fix build scripts

--- buildscripts/post_image.sh
+++ buildscripts/post_image.sh
@@ -3,15 +3,18 @@ set -e
 
 WORKSPACE=${GITHUB_WORKSPACE:-/src}
 
+OUT_DIR=$WORKSPACE/output
+[ -f $OUT_DIR ] || mkdir $OUT_DIR
+
 cd output/images
 echo "atomaskygaze" > hostname
 touch authorized_keys
 cp -dpf uImage.lzma factory_t31_ZMC6tiIDQN
 mv rootfs.squashfs rootfs_hack.squashfs
-cp -f factory_t31_ZMC6tiIDQN rootfs_hack.squashfs $WORKSPACE/output
-cp -r $WORKSPACE/assets $WORKSPACE/output
+cp -f factory_t31_ZMC6tiIDQN rootfs_hack.squashfs $OUT_DIR
+cp -r $WORKSPACE/assets $OUT_DIR
 
-cd $WORKSPACE/output
+cd $OUT_DIR
 VERSION=$(cat $WORKSPACE/configs/skygaze.ver)
 ZIP_NAME="$WORKSPACE/atom-skygaze.v$VERSION.zip"
 [ -f $ZIP_NAME ] && rm "$ZIP_NAME"
